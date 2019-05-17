42f18fa956ad5fc02c593336e4c2a1c494ff1a36
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-17 16:32:58 -0500

    Create script for backend redeployment

--- redeploy.sh
+++ redeploy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+az vm extension set \
+  --resource-group kellma \
+  --vm-name myVM \
+  --name customScript \
+  --publisher Microsoft.Azure.Extensions \
+  --settings '{"fileUris": ["https://raw.githubusercontent.com/me/project/hello.sh"],"commandToExecute": "./hello.sh"}'
