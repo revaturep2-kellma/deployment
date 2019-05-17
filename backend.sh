3556a66806a36b849f7b5fbc19b4afaaae747c99
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-17 17:25:19 -0500

    create file during script for checking

--- backend.sh
+++ backend.sh
@@ -5,6 +5,10 @@
 
 killall node
 cd /media/app/kellmas-backend
+touch somerandomfile.txt
 git pull
+touch somerandomfile2.txt
 npm install
+touch somerandomfile3.txt
 npm start
+touch somerandomfile4.txt
